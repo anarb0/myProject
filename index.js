de2f16bd5c6c5a6c701baf7c7609254f7b529648
anarb0 <anarb359@gmail.com>
2024-07-31 11:44:56 -0400

    edit box pop up

---
+++ index.js
@@ -0,0 +1,8 @@
+function edit () {
+    const editBoxEls = document.getElementsByClassName('edit-box');
+
+    console.log(editBoxEls[0]);
+    editBoxEls[0].style.zIndex = 1
+}
+
+let arr = [2, 4];
