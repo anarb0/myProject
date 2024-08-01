d284a88909d1930a670bca22ac94ba04cafd45b4
anarb0 <anarb359@gmail.com>
2024-08-01 12:06:01 -0400

    background color change

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function edit () {
     const editBoxEls = document.getElementsByClassName('edit-box');
-    console.log(editBoxEls[0]);
+    // const hiddenMenu = docume.getElementsByClassName('hidden-dropdown-menu');
+    // hiddenMenu[0].style.display = 'none'
     editBoxEls[0].style.display = 'block'
+    const dim = document.getElementById('dim');
+    dim.style.zIndex = 8;
 }
 function deleteClick () {
     const deleteBoxEls = document.getElementsByClassName('delete-box');
@@ -13,8 +16,19 @@ function cancel (event) {
     let className = event.target.id.split('-')[0]+'-box'
     const cancelButtonEls = document.getElementsByClassName(className);
     cancelButtonEls[0].style.display = 'none'
+    const dim = document.getElementById('dim');
+    dim.style.zIndex = -100;
 }
+
+
 function add () {
     const addTaskBoxEls = document.getElementsByClassName('task-add-box');
     addTaskBoxEls[0].style.display = 'block'
+}
+
+function everywhereCancel () {
+    const dim = document.getElementById('dim');
+    dim.style.zIndex = -100;
+    const editBoxEls = document.getElementsByClassName('edit-box');
+    editBoxEls[0].style.display = 'none'
 }
